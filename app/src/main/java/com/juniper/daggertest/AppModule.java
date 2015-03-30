3b9459768fd994bea22d76bdf850c139ce02684f
unknown <rtejasvi@juniper.net>
2015-03-30 16:32:18 +0530

    Try to get pp context

--- app/src/main/java/com/juniper/daggertest/AppModule.java
+++ app/src/main/java/com/juniper/daggertest/AppModule.java
@@ -1,8 +1,12 @@
 package com.juniper.daggertest;
 
+import android.content.Context;
+
 import com.juniper.daggertest.service.Test;
 import com.juniper.daggertest.service.TestImpl;
 import com.juniper.daggertest.ui.MainActivity;
+
+import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
@@ -21,6 +25,11 @@ public class AppModule {
     public AppModule(App app){
         this.app = app;
     }
+
+    @Provides @Singleton public Context provideApplicationContext() {
+        return app.getApplicationContext();
+    }
+
 
     @Provides
     public Test providesTestService(){
--- app/src/main/java/com/juniper/daggertest/service/TestImpl.java
+++ app/src/main/java/com/juniper/daggertest/service/TestImpl.java
@@ -1,13 +1,22 @@
 package com.juniper.daggertest.service;
 
+import android.content.Context;
+
+import javax.inject.Inject;
+
 /**
  * Created by rtejasvi on 3/30/2015.
  */
 public class TestImpl implements Test {
 
+    @Inject
+    Context ctx;
 
     @Override
     public String getString() {
+        if(ctx==null){
+            return "Dagger being called, but context still null";
+        }
         return "Hey, It's a success!";
     }
 }
